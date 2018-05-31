9e1242a880c3f9adcc777a213544254fec0a02df
Akkikumar <akkikumar625@gmail.com>
2018-05-31 14:54:37 +0530

    Update Test.java

--- Test_Project/src/com/trinity/Test.java
+++ Test_Project/src/com/trinity/Test.java
@@ -14,7 +14,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hai check for commit");
+		System.out.println("Hai check for update");
 		String s="1.18112e+06";
 		System.out.println(s.contains("e+"));
 	}
