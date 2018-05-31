2cff27ebf536014ae57846e349b1e127682e76ef
tmpl1705 <akshayuttur@outlook.com>
2018-05-31 14:52:43 +0530

    sop changes

--- Test_Project/src/com/trinity/Test.java
+++ Test_Project/src/com/trinity/Test.java
@@ -14,7 +14,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hai Veda");
+		System.out.println("Hai check for commit");
 		String s="1.18112e+06";
 		System.out.println(s.contains("e+"));
 	}
